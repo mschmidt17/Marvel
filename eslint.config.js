d8b91c70026412e9165df90947e2b855434a0d63
matias2 <matias.schmidt@outlook.es>
2025-08-24 08:44:22 -0300

    feat: se crea sub carpetas dentro de assets para mayor visualizacion de imagenes

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,45 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import { globalIgnores } from 'eslint/config';
 
-export default tseslint.config([
+export default [
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
+    parser: tsParser,
+    parserOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      ecmaFeatures: { jsx: true },
+    },
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      '@typescript-eslint': ts,
+      'react-refresh': reactRefresh,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-refresh/recommended',
+      'plugin:prettier/recommended',
+    ],
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      // reglas personalizadas aquí
     },
   },
-])
+];
