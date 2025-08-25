3316fbd7191e30b69094c2cb560b2f76a7689fc1
matias2 <matias.schmidt@outlook.es>
2025-08-25 19:09:56 -0300

    fix: se ajusta id

--- src/hooks/useFetchComics.ts
+++ src/hooks/useFetchComics.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import type { Comic } from '../api/marvelApi';
 import { getComicsByCharacterId } from '../api/marvelApi';
 
-export const useFetchComics = (characterId: number) => {
+export const useFetchComics = (characterId?: number) => {
   const [comics, setComics] = useState<Comic[]>([]);
   const [loading, setLoading] = useState(false);
 
